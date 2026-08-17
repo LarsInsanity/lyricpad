9f3c2690baaf050ee47f2968d0f858bfd91503e3
LarsInsanity <143612852+LarsInsanity@users.noreply.github.com>
2026-08-18 01:44:13 +0200

    good

--- LyricPad_Next_v0.6.1/run_lyricpad_next.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if not exist ".env" copy ".env.example" ".env" >nul
-start "" http://localhost:8787
-node server.mjs
-pause
--- LyricPad_Next_v0.6.1/setup_phone_https.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0setup_phone_https.ps1"
